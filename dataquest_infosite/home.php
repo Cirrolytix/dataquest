ac15fdffb178a29ad3412a7349ae139cb466092c
Rache <rachemelendres@gmail.com>
2022-07-11 19:52:26 +0800

    updated site

--- dataquest_infosite/home.php
+++ dataquest_infosite/home.php
@@ -74,24 +74,24 @@ function showHome() {
         </div>
         <div class="w3-row">
             
-            <a href="#awards" onclick="toggle('popup-education');" class="w3-third w3-padding-large w3-center">
+            <a href="#awards" class="w3-third w3-padding-large w3-center">
                 <img src="images/award1.png" style="width: 85%;" alt="Best Use of Data" />
                 <p class="w3-padding-medium w3-large">Best Use of Data</p>
             </a>
-            <a href="#awards" onclick="toggle('popup-peace');" class="w3-third w3-padding-large w3-center">
+            <a href="#awards" class="w3-third w3-padding-large w3-center">
                 <img src="images/award2.png" style="width: 85%;" alt="Best Use of Data Science" />
                 <p class="w3-padding-medium w3-large">Best Use of Data Science</p>
             </a>
-            <a href="#awards" onclick="toggle('popup-economy');" class="w3-third w3-padding-large w3-center">
+            <a href="#awards" class="w3-third w3-padding-large w3-center">
                 <img src="images/award3.png" style="width: 85%;" alt="Most Relevant to Migration Labor and Governance " />
                 <p class="w3-padding-medium w3-large">Most Relevant to Migration Labor and Governance </p>
             </a></div>
             <div class="w3-row">
-            <a href="#awards" onclick="toggle('popup-health');" class="w3-half w3-padding-large w3-center">
+            <a href="#awards" class="w3-half w3-padding-large w3-center">
                 <img src="images/award4.png" style="width: 55%;" alt="Most Practical Solution" />
                 <p class="w3-padding-medium w3-large">Most Practical Solution</p>
             </a>
-            <a href="#awards" onclick="toggle('popup-health');" class="w3-half w3-padding-large w3-center">
+            <a href="#awards" class="w3-half w3-padding-large w3-center">
                 <img src="images/award5.png" style="width: 55%;" alt="Most Gender-Responsive" />
                 <p class="w3-padding-medium w3-large">Most Gender-Responsive</p>
             </a>
@@ -166,95 +166,6 @@ function showHome() {
         </div>
     </section>
  
-    <!-- Popups -->
-    <section id="popup-education" class="w3-modal w3-hide" onclick="toggleClose('popup-education');">
-        <div class="w3-modal-content">
-            <div class="w3-black w3-bar">
-                <div class="w3-bar-item">
-                    <img src="images/barmm2_black.jpg" height=40 alt="BARMM Data Challenge" />
-                </div>
-            </div>
-            <div id="theme-education" class="w3-padding">
-                <div class="w3-row">
-                    <div class="w3-col l1">
-                        <img src="images/sdg_4.jpg" style="width: 100%;" alt="SDG 4" />
-                    </div>
-                    <div class="w3-col l11 w3-padding w3-text-white w3-small">
-                        <h2 class="w3-large">How can data and technology help in achieving INCLUSIVE EDUCATION for all?</h2>
-                        <p>Inclusive education is envisioned as part of the priorities for the Bangsamoro Education system led by the Ministry of Basic, Higher and Technical Education. It means that the education system shall have a broad, relevant, and inclusive system accessible to all students--whether Moro, Settlers, or IP. It shall also guarantee an equal opportunity for all. It is a public knowledge supported by data on learning outcomes that the quality of education in BARMM is not exemplary. This entails creating a safe and inclusive school environment for children with disabilities, cultural groups and other special circumstances. Conducting a needs assessment, developing clear guidelines and ensuring specialized support must be implemented to better respond to their situations.</p>
-                        <p>Access to the data is important to provide relevant information for the education policy actors to improve access to quality and inclusive education opportunities for all children. There is a need to address issues on the monitoring and evaluation of special education (SPED) programs and teachers, integration of Madrasah education, review and contextualizing of curriculum specific to the region (including approaches with the ethnic groups) mapping SPED schools in all divisions, mapping of out of school children and youth. These examples and many more issues in the education sector need to be shown in an easily comprehensible way to the intended audience to help them make informed decisions (map, dashboard, interactive database, etc.).</p>
-                    </div>
-                </div>
-            </div>
-            <div class="w3-black w3-padding w3-small">&copy; 2020-2021 CirroLytix Research Services</div>
-        </div>
-    </section>
-    <section id="popup-peace" class="w3-modal w3-hide" onclick="toggleClose('popup-peace');">
-        <div class="w3-modal-content">
-            <div class="w3-black w3-bar">
-                <div class="w3-bar-item">
-                    <img src="images/barmm2_black.jpg" height=40 alt="BARMM Data Challenge" />
-                </div>
-            </div>
-            <div id="theme-peace" class="w3-padding">
-                <div class="w3-row">
-                    <div class="w3-col l1">
-                        <img src="images/sdg_16.jpg" style="width: 100%;" alt="SDG 16" />
-                    </div>
-                    <div class="w3-col l11 w3-padding w3-text-white w3-small">
-                        <h2 class="w3-large">How can data and technology help in promoting MORAL GOVERNANCE?</h2>
-                        <p>Reforms in the governance structure are required in establishing moral governance. The challenge lies in the restoration of people's confidence in the new government, including its justice system. The experiences of previous autonomous governments shall pave the way for a new government that is responsive, transparent, accountable, and participative. Responsive policies and programs must be in place. Enabling the environment for participatory procedures shall promote the important role of CSOs representing various sectors.</p>
-                        <p>The high expectations for the new government remain a problem. Increased access to information and transparency of government efforts must remain a priority. Honest and competent public officials shall be seated and undergo relevant capacity building training and values formation sessions. The monitoring and evaluation system must be built in every stage of the bureaucratic structure. An effective, people-centered feedback mechanism also needs to be in place to ensure government efforts are felt and relevant to the needs of its constituents. Trends in the use of technology need to be maximized to interpret moral governance to the ordinary citizen. A truly democratic governance in BARMM shall be achieved with vibrant civil society participation and citizens' support. Access to reliable data is crucial for people’s participation. All these elements are anchored to the Chief Minister’s policy direction and commitment for moral governance.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="w3-black w3-padding w3-small">&copy; 2020-2021 CirroLytix Research Services</div>
-        </div>
-    </section>
-    <section id="popup-economy" class="w3-modal w3-hide" onclick="toggleClose('popup-economy');">
-        <div class="w3-modal-content">
-            <div class="w3-black w3-bar">
-                <div class="w3-bar-item">
-                    <img src="images/barmm2_black.jpg" height=40 alt="BARMM Data Challenge" />
-                </div>
-            </div>
-            <div id="theme-economy" class="w3-padding">
-                <div class="w3-row">
-                    <div class="w3-col l1">
-                        <img src="images/sdg_1.jpg" style="width: 100%;" alt="SDG 1" />
-                    </div>
-                    <div class="w3-col l11 w3-padding w3-text-white w3-small">
-                        <h2 class="w3-large">How can data and technology help in ALLEVIATING POVERTY and BOOSTING ECONOMIC DEVELOPMENT?</h2>
-                        <p>The unprecedented impact of COVID-19 has taken its toll on the national economy and mostly felt in the poorest and rural areas in Mindanao. Prior to the enactment of the Bangsamoro Organic Law (BOL), BARMM's performance is the poorest in the country in terms of human development index. In 2018, its poverty incidence was 55.4% reaching a high per capita poverty threshold of PhP13,578 and per capita food threshold of PhP9,565.</p>
-                        <p>As the means to alleviating poverty, ramping up efforts to boost the region's tourism and micro and small enterprises is vital to not backslide any achieved gains. Both sectors hold a significant part in the economy of Mindanao, which have boosted its economic development and provided employment and generated wealth for BARMM. To ease the impact to the supply chain and regional economy, government actors are rushing their way to actualize sound judgements to support affected sectors. There is a need to provide relevant information on providing economic opportunities and pivot towards innovative and speedy solutions to accurately respond with the new normal scenario. The agriculture sector, which remains resilient amidst the impact of COVID-19, is one of the potential segments to boost economic recovery in the region, hence a need for data-driven analysis for both agriculture and fishery industries are necessary.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="w3-black w3-padding w3-small">&copy; 2020-2021 CirroLytix Research Services</div>
-        </div>
-    </section>
-    <section id="popup-health" class="w3-modal w3-hide" onclick="toggleClose('popup-health');">
-        <div class="w3-modal-content">
-            <div class="w3-black w3-bar">
-                <div class="w3-bar-item">
-                    <img src="images/barmm2_black.jpg" height=40 alt="BARMM Data Challenge" />
-                </div>
-            </div>
-            <div id="theme-health" class="w3-padding">
-                <div class="w3-row">
-                    <div class="w3-col l1">
-                        <img src="images/sdg_3.jpg" style="width: 100%;" alt="SDG 3" />
-                    </div>
-                    <div class="w3-col l11 w3-padding w3-text-white w3-small">
-                        <h2 class="w3-large">How can data and technology help in COVID-19 RESPONSE and RECOVERY?</h2>
-                        <p>The regional government has put in place health and safety measures consistent with the national guidelines for the management of the spread of COVID-19. On 16 March 2020, President Rodrigo Roa Duterte signed Proclamation No. 929 Declaring a State of Public Health Emergency throughout the country for six (6) months mandating all government agencies and local government units (LGUs) to mobilize the necessary resources to undertake appropriate response aid and measures to eliminate COVID-19.</p>
-                        <p>It is vital that communities at the grassroots level are informed about COVID-19. Relevant information allows them to make informed actions on how to protect their families and control its spread. Hence, available datasets on the spread of the disease and preventive measures are needed to be translated in an appealing way to ordinary citizens, considering area-specific nuances. However, much of the burden in controlling the transmission of the virus is on the shoulders of local government leaders-relying on government-initiated efforts on the conduct of quick and systematic contact tracing and wide testing. Thus, community leaders must be equipped in responding rapidly. Aside from this, innovations are vital to improve the quality of life of internally displaced persons (IDPs) in Marawi who have reported lack of health services (e.g. medical missions, medical clinics, affordable medicines) in their relocation sites. Best practices that can be replicated include barangay-based community management, relief and economic stimulus systems, and COVID-19 emergency maps.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="w3-black w3-padding w3-small">&copy; 2020-2021 CirroLytix Research Services</div>
-        </div>
-    </section>
-    
+       
     <?php
 }
