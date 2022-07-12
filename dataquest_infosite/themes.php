ef73bdda8904bcd831d0826a3bb02b9c8686a57d
Dominic Ligot <dominic.ligot@gmail.com>
2022-07-12 17:58:00 +0800

    update themes

--- dataquest_infosite/themes.php
+++ dataquest_infosite/themes.php
@@ -2,81 +2,77 @@
 function showThemes() {
 ?>
 
-<section id="themes" class="w3-padding-128 w3-highway-blue w3-padding">
-      <div class="w3-row">
-        <div style="height: 59px;"></div>
+    <section id="themes" class="w3-padding-128 w3-highway-blue w3-padding">
+        <div class="w3-row">
+            <div style="height: 59px;"></div>
 
-        <div id = "theme1" class="w3-row">
-            <a href="#theme1" class="w3-col l6 w3-center">
-           <img src="images/theme1_bw.jpg" style="width: 65%" alt="Fair, ethical and gender responsive recruitment of OFW"/> 
-           <p class="w3-small"><i>Photo by KC Wong</i></p>
-          </a>
-          
-               
+            <div id="theme1" class="w3-row">
+                <a href="#theme1" class="w3-col l6 w3-center">
+                    <img src="images/theme1_bw.jpg" style="width: 65%" alt="Fair, ethical and gender responsive recruitment of OFW" />
+                    <p class="w3-small"><i>Photo by KC Wong</i></p>
+                </a>
 
-            <h3 class="w3-col l6 w3-left">Themes</h3>
-            <h1 class="w3-col l5 w3-left" style="color:#9fc5e8"><b>Fair, ethical and gender responsive recruitment of OFW</b></h1>
-            <p class="w3-col l5 w3-left">Seventy percent of human trafficking victims assisted by the Department of Social Welfare and Development (DSWD) in 2020 were women. <a href="#sources"> [1] </a> Incidences of illegal recruitment and trafficking were expected to increase amid the economic impacts of COVID-19. How can this be avoided, and how do we ensure that women and other would-be migrant workers benefit from fair and ethical recruitment?</p>     
-        </div> 
+                <h3 class="w3-col l6 w3-left">Themes</h3>
+                <h1 class="w3-col l5 w3-left" style="color:#9fc5e8"><b>Fair, ethical and gender responsive recruitment of OFW</b></h1>
+                <p class="w3-col l5 w3-left">Seventy percent of human trafficking victims assisted by the Department of Social Welfare and Development (DSWD) in 2020 were women. <a href="#sources"> [1] </a> Incidences of illegal recruitment and trafficking were expected to increase amid the economic impacts of COVID-19. How can this be avoided, and how do we ensure that women and other would-be migrant workers benefit from fair and ethical recruitment?</p>
+            </div>
 
-        <div style="height: 59px;"></div>
+            <div style="height: 59px;"></div>
 
-        <div id = "theme2" class="w3-row">
-          <a href="#theme2" class="w3-col l6 w3-center">
-          <img src="images/theme2_bw.jpg" style="width: 65%" alt="Decent work in destination countries for women"/> 
-          <p class="w3-small"><i>Photo by ILO Asia Pacific</i></p>
-        </a>
-          <h3 class="w3-col l6 w3-left">Themes</h3>
-          <h1 class="w3-col l5 w3-left" style="color:#9fc5e8"><b>Decent work in destination countries for women</b></h1>
-          <p class="w3-col l5 w3-left">Majority of women OFWs work in elementary occupations, <a href="#sources"> [2] </a> which include domestic work, where they receive low wages and limited social protection. Many of them work inside households, where work conditions are difficult to monitor. How can decent work be assured in these scenarios?</p>
-          
-      
-          </div> 
+            <div id="theme2" class="w3-row">
+                <a href="#theme2" class="w3-col l6 w3-center">
+                    <img src="images/theme2_bw.jpg" style="width: 65%" alt="Decent work in destination countries for women" />
+                    <p class="w3-small"><i>Photo by ILO Asia Pacific</i></p>
+                </a>
+                <h3 class="w3-col l6 w3-left">Themes</h3>
+                <h1 class="w3-col l5 w3-left" style="color:#9fc5e8"><b>Decent work in destination countries for women</b></h1>
+                <p class="w3-col l5 w3-left">Majority of women OFWs work in elementary occupations, <a href="#sources"> [2] </a> which include domestic work, where they receive low wages and limited social protection. Many of them work inside households, where work conditions are difficult to monitor. How can decent work be assured in these scenarios?</p>
 
-          <div style="height: 59px;"></div>
+            </div>
 
-        <div id = "theme3" class="w3-row">
-          <a href="#theme3" class="w3-col l6 w3-center">
-          <img src="images/theme3_bw.jpg" style="width: 65%" alt="Sustainable and gender-responsive reintegration of returning OFWs"/>
-          <p class="w3-small"><i>Photo by UN Women, Staton Winter</i></p>
-        </a>
-          <h3 class="w3-col l6 w3-left">Themes</h3>
-          <h1 class="w3-col l5 w3-left" style="color:#9fc5e8"><b>Sustainable and gender-responsive reintegration of returning OFWs</b></h1>
-          <p class="w3-col l5 w3-left">The International Organization for Migration (IOM) defines sustainable reintegration as the point when a migrant worker has achieved “levels of economic self-sufficiency, social stability, and psychosocial well-being that make their further migration decisions a matter of choice, rather than necessity.” <a href="#sources"> [3] </a> How can we facilitate this for women OFWs or OFWs with diverse SOGIE?</p>
-          
-      
-          </div> 
-          <div style="height: 59px;"></div>
+            <div style="height: 59px;"></div>
 
-        <div id = "theme4" class="w3-row">
-          <a href="#theme4" class="w3-col l6 w3-center">
-          <img src="images/theme4_bw.jpg" style="width: 65%" alt="Making labor migration more gender-responsive post-pandemic"/> <p class="w3-small"><i>Photo by UN Women</i></p></a>
-          <h3 class="w3-col l6 w3-left">Themes</h3>
-          <h1 class="w3-col l5 w3-left" style="color:#9fc5e8"><b>Making labor migration more gender-responsive post-pandemic</b></h1>
-          <p class="w3-col l5 w3-left">The COVID-19 pandemic revealed the unequal care burden of women and men, including the disproportionate representation of women in health and social care, where they comprise 70 percent of workers. <a href="#sources">[4]</a> How can we translate this realization into action that will benefit women and gender nonconforming migrant workers?</p>
-          
-      
-          </div> 
-     
-     <div style="height: 59px;"></div>
-   <div id = "sources" class="w3-row w3-left">
-      <h5 class="w3-col l6 w3-left w3-center"><b>Sources</b></h5>
-    <div class="w3-row w3-left">
-      <!-- <div class="w3-col l3 w3-center">&nbsp</div> -->
-			<p class="w3-col l9 w3-right w3-small"> [1] <a href="https://www.state.gov/reports/2021-trafficking-in-persons-report/philippines_trashed/">https://www.state.gov/reports/2021-trafficking-in-persons-report/philippines_trashed/ </a><br>
+            <div id="theme3" class="w3-row">
+                <a href="#theme3" class="w3-col l6 w3-center">
+                    <img src="images/theme3_bw.jpg" style="width: 65%" alt="Sustainable and gender-responsive reintegration of returning OFWs" />
+                    <p class="w3-small"><i>Photo by UN Women, Staton Winter</i></p>
+                </a>
+                <h3 class="w3-col l6 w3-left">Themes</h3>
+                <h1 class="w3-col l5 w3-left" style="color:#9fc5e8"><b>Sustainable and gender-responsive reintegration of returning OFWs</b></h1>
+                <p class="w3-col l5 w3-left">The International Organization for Migration (IOM) defines sustainable reintegration as the point when a migrant worker has achieved “levels of economic self-sufficiency, social stability, and psychosocial well-being that make their further migration decisions a matter of choice, rather than necessity.” <a href="#sources"> [3] </a> How can we facilitate this for women OFWs or OFWs with diverse SOGIE?</p>
 
-          [2] <a href="https://psa.gov.ph/statistics/survey/labor-and-employment/survey-overseas-filipinos">https://psa.gov.ph/statistics/survey/labor-and-employment/survey-overseas-filipinos </a> <br>
+            </div>
+            <div style="height: 59px;"></div>
 
-          [3] <a href="https://publications.iom.int/books/reintegration-handbook-practical-guidance-design-implementation-and-monitoring-reintegration"> https://publications.iom.int/books/reintegration-handbook-practical-guidance-design-implementation-and-monitoring-reintegration </a><br>
+            <div id="theme4" class="w3-row">
+                <a href="#theme4" class="w3-col l6 w3-center">
+                    <img src="images/theme4_bw.jpg" style="width: 65%" alt="Making labor migration more gender-responsive post-pandemic" />
+                    <p class="w3-small"><i>Photo by UN Women</i></p>
+                </a>
+                <h3 class="w3-col l6 w3-left">Themes</h3>
+                <h1 class="w3-col l5 w3-left" style="color:#9fc5e8"><b>Making labor migration more gender-responsive post-pandemic</b></h1>
+                <p class="w3-col l5 w3-left">The COVID-19 pandemic revealed the unequal care burden of women and men, including the disproportionate representation of women in health and social care, where they comprise 70 percent of workers. <a href="#sources">[4]</a> How can we translate this realization into action that will benefit women and gender nonconforming migrant workers?</p>
 
-          [4] <a href = "https://www.unwomen.org/sites/default/files/Headquarters/Attachments/Sections/Library/Publications/2021/Feminist-plan-for-sustainability-and-social-justice-en.pdf">https://www.unwomen.org/sites/default/files/Headquarters/Attachments/Sections/Library/Publications/2021/Feminist-plan-for-sustainability-and-social-justice-en.pdf</a></p>
-        </div>
-      </div>
-   
-      
- </section>
+            </div>
+			
+			<div id="sources" style="padding-top: 64px;">
+				<div class="w3-quarter">&nbsp;</div>
+				<div class="w3-half">
+					<h5><b>Sources</b></h5>
+					
+					<ol>
+						<li><a href="https://www.state.gov/reports/2021-trafficking-in-persons-report/philippines_trashed/">2021 Traficking in Persons Report, US Department of State</a></li>
+						<li><a href="https://psa.gov.ph/statistics/survey/labor-and-employment/survey-overseas-filipinos">2020 Overseas Filipino Workers (Final Results)</a></li>
+						<li><a href="https://publications.iom.int/books/reintegration-handbook-practical-guidance-design-implementation-and-monitoring-reintegration">Reintegration Handbook - Practical guidance on the design, implementation and monitoring of reintegration assistance</a></li>
+						<li><a href = "https://www.unwomen.org/sites/default/files/Headquarters/Attachments/Sections/Library/Publications/2021/Feminist-plan-for-sustainability-and-social-justice-en.pdf">UN Women, A Feminist Plan for Sustainability and Social Justice</a></li>
+					</ol>
+				</div>
+				<div class="w3-quarter">&nbsp;</div>
+			
+			
+			</div>
 
+    </section>
 
-
-<?php
+    <?php
 }
