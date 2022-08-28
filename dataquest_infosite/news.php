5b822a495d3f813db4a83fc7b8d704a4d32f6855
Rache <rachemelendres@gmail.com>
2022-08-28 11:38:49 +0800

    update news

--- dataquest_infosite/news.php
+++ dataquest_infosite/news.php
@@ -12,6 +12,13 @@
           <!-- <div style="height: 40px;"></div> -->
 
           <div class="w3-row-padding w3-padding-16 w3-center">
+
+                 <a href="https://dataquest.opendata.org.ph/community/discussion/60/dataquest-hackatalk-series-a-webinar-series-in-support-of-hackathon-participants" class="w3-third" >
+                  <img src="images/news-3.jpg" style="width: 40%;" alt="Dataquest HackaTalk Series: A Webinar Series in Support of Hackathon Participants" />
+                  <h4 style= "text-align:justify; padding:0px 50px 0px"><b>Dataquest HackaTalk Series: A Webinar Series in Support of Hackathon Participants</b></h4>
+                  <p style= "text-align:justify; padding:0px 50px 0px"><b>August 27, 2022</b></p>
+                  <p style= "text-align:justify; padding:0px 50px 0px">CirroLytix and Data Ethics PH, in partnership with Foundation for Media Alternatives and the BRIDGE Program, recently held the Dataquest HackaTalk series, last August 19, 24, and 26. This event series was held in support of existing and would-be hackathon participants, to help them better understand the OFW challenges, network with the Dataquest community, and interact with the mentors.</p>
+              </a>
               
               <a href="https://dataquest.opendata.org.ph/community/discussion/54/dataquest-hackatalk-series" class="w3-third" >
                   <img src="images/news-2.jpg" style="width: 70%;" alt="CirroLytix Launches Dataquest 2022: Gender-Responsive Labor Migration" />
@@ -26,12 +33,7 @@
                   <p style= "text-align:justify; padding:0px 50px 0px"><b>July 20, 2022</b></p>
                   <p style= "text-align:justify; padding:0px 50px 0px">Last 19 July 2022, CirroLytix formally launched Dataquest 2022: Gender-Responsive Labor Migration, in partnership with Data Ethics PH, Foundation for Media Alternatives (FMA), and the BRIDGE programme, jointly implement by UN Women, International Organization for Migration (IOM), and International Labour Organization (ILO). The hackathon is aimed at crowdsourcing innovative data-driven solutions to address problems and issues related to the Philippine labor migration sector.</p>
               </a>
-              <!-- <a href="#awards" class="w3-third" >
-                  <img src="images/news-1.png" style="width: 70%;" alt="CirroLytix Launches Dataquest 2022: Gender-Responsive Labor Migration" />
-                  <h4 style= "text-align:justify; padding:0px 50px 0px">CirroLytix Launches Dataquest 2022: Gender-Responsive Labor Migration</h4>
-                  <p style= "text-align:justify; padding:0px 50px 0px"><b>July 20, 2022</b></p>
-                  <p style= "text-align:justify; padding:0px 50px 0px">Last 19 July 2022, CirroLytix formally launched Dataquest 2022: Gender-Responsive Labor Migration, in partnership with Data Ethics PH, Foundation for Media Alternatives (FMA), and the BRIDGE programme, jointly implement by UN Women, International Organization for Migration (IOM), and International Labour Organization (ILO). The hackathon is aimed at crowdsourcing innovative data-driven solutions to address problems and issues related to the Philippine labor migration sector.</p>
-              </a> -->
+       
             
             </div>
   </section>
